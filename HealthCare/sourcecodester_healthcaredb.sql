4d1ffc02f159e1adb5b3dada7011d7a456493853
SWETHA464 <142730993+SWETHA464@users.noreply.github.com>
2023-12-03 20:38:49 +0530

    Update sourcecodester_healthcaredb.sql

--- HealthCare/sourcecodester_healthcaredb.sql
+++ HealthCare/sourcecodester_healthcaredb.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.8.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Oct 17, 2020 at 01:26 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 5.6.39
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
@@ -19,7 +10,6 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sourcecodester_healthcaredb`
 --
 
 -- --------------------------------------------------------
